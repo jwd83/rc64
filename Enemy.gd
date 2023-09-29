e51e9de289095cd306c39d3a39843526376aa6ee
Jared De Blander <jared0x90@gmail.com>
2023-09-29 15:36:23 -0400

    working on improving the starfield

--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-var movement_speed: float = randf_range(40,80)
+var movement_speed: float = randf_range(60,100)
 
 func _physics_process(delta):
 	velocity.z = movement_speed
